6b7bf1add911944f6df6351bc5229076c29a2b98
vedtam <vedtam@yahoo.com>
2016-10-29 12:40:08 +0300

    uploaded

---
+++ imports/startup/client/routes.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import { App } from '../../ui/layouts/App.jsx';
+import { Index } from '../../ui/components/index.jsx';
+
+import { One } from '../../ui/pages/one.jsx';
+import { Two } from '../../ui/pages/two.jsx';
+
+import { Hello } from '../../ui/pages/hello.jsx';
+import { NotFound } from '../../ui/pages/not-found.jsx';
+
+Meteor.startup( () => {
+  render(
+    <Router history={ browserHistory }>
+      <Route path="/" component={ App }>
+        <IndexRoute component={ Index } />
+        <Route path="/one" component={ One } />
+        <Route path="/two" component={ Two } />
+        <Route path="/hello/:name" component={ Hello } />
+      </Route>
+      <Route path="*" component={ NotFound } />
+    </Router>,
+    document.getElementById( 'react-root' )
+  );
+});
